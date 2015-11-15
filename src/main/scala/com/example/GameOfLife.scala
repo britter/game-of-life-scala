82ae3b7f81d5f9636e8a3d6ba6fcd7c9cbe079a7
Benedikt Ritter <beneritter@gmail.com>
2015-11-15 12:54:01 +0100

    Revert Round 3

--- src/main/scala/com/example/GameOfLife.scala
+++ src/main/scala/com/example/GameOfLife.scala
@@ -6,27 +6,4 @@ object GameOfLife {
     println("Hello, world!")
   }
 
-  case class Point(x: Int, y: Int) {
-    def isNeighbour(other: Point): Boolean = {
-      val distX = Math.abs(other.x - this.x)
-      val distY = Math.abs(other.y - this.y)
-      distX < 2 && distY < 2 && other != this
-    }
-    
-    def getAllNeighbours(): Set[Point] = {
-      for(x <- Set(-1, 0, 1);
-          y <- Set(-1, 0, 1)
-          if x != 0 && y != 0) yield Point(this.x + x, this.y + y)
-    }
-  }
-
-  case class World(initial: Set[Point]) {
-    def getNextGeneration() = {
-      World(initial.filter(point => countLivingNeighbours(point) > 1))
-    }
-
-    def countLivingNeighbours(point: Point) = {
-      initial.count(each => each.isNeighbour(point))
-    }
-  }
 }
--- src/test/scala/com/example/GameOfLifeSpec.scala
+++ src/test/scala/com/example/GameOfLifeSpec.scala
@@ -1,46 +1,11 @@
 package com.example
 
-import com.example.GameOfLife.{Point, World}
 import org.scalatest._
 
 class GameOfLifeSpec extends FlatSpec with Matchers {
 
-  val emptyWorld = World(Set())
-  val oneCellWorld = World(Set(Point(0,0)))
-  val staticWorld = World(Set(Point(0,0), Point(0,1), Point(1,0), Point(1,1)))
-
-  "getNextGeneration" should "return empty world for empty world" in {
-    emptyWorld.getNextGeneration() should === (emptyWorld)
+  "hello" should "be true" in {
+    true should === (true)
   }
-
-  it should "return empty world if it is under populated" in {
-
-    oneCellWorld.getNextGeneration() should === (emptyWorld)
-  }
-
-  it should "return static world" in {
-
-    staticWorld.getNextGeneration() should === (staticWorld)
-  }
-
-  it should "return dead world for starving world" in {
-    val starvingWorld = World(Set(Point(0,0), Point(1,1)))
-
-    starvingWorld.getNextGeneration() should === (emptyWorld)
-  }
-
-  it should "return world with new cells" in {
-    val reproductiveWorld = World(Set(Point(0,0), Point(0,1), Point(1,1)))
-
-    reproductiveWorld.getNextGeneration() should === (staticWorld)
-  }
-
-  "countLivingNeighbours" should "return 0 for empty world" in {
-    emptyWorld.countLivingNeighbours(Point(0,0)) should === (0)
-  }
-
-  it should "return one for a point wiht one neighbour" in {
-    oneCellWorld.countLivingNeighbours(Point(0,1)) should === (1)
-  }
-
+  
 }

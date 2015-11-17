2c4d9ed70a81ab00ea114e268d7975381edd6e64
Benedikt Ritter <beneritter@gmail.com>
2015-11-17 21:15:36 +0100

    Revert everything for next round

--- src/main/scala/com/example/GameOfLife.scala
+++ src/main/scala/com/example/GameOfLife.scala
@@ -2,31 +2,6 @@ package com.example
 
 object GameOfLife {
 
-  type Cell = (Int, Int)
-
-  def evolve(world: Set[Cell]) = {
-    val survivors = for (
-      cell <- world;
-      livingNeighbours = neighbours(cell).filter(cell => world.contains(cell))
-      if livingNeighbours.size == 2 || livingNeighbours.size == 3
-    ) yield cell
-
-    val newBorn = world.toList.flatMap(neighbours).groupBy(identity).mapValues(_.size).filter(_._2 == 3).keySet
-    newBorn ++ survivors
-  }
-
-  val neighbours = (cell: Cell) => {
-    for (
-      x <- List(-1, 0, 1);
-      y <- List(-1, 0, 1)
-      if x != 0 || y != 0
-    ) yield (cell._1 + x, cell._2 + y)
-  }
-
-  val occurences = (neighbours: List[Cell]) => {
-    neighbours.groupBy(cell => cell).mapValues(_.size)
-  }
-
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
--- src/test/scala/com/example/GameOfLifeSpec.scala
+++ src/test/scala/com/example/GameOfLifeSpec.scala
@@ -1,32 +1,11 @@
 package com.example
 
-import com.example.GameOfLife.neighbours
-import com.example.GameOfLife.occurences
 import org.scalatest._
 
 class GameOfLifeSpec extends FlatSpec with Matchers {
 
-  "evolve" should "create empty world for empty world" in {
-    GameOfLife.evolve(Set()) should === (Set())
+  "true" should "equal true" in {
+    true should === (true)
   }
 
-  it should "create a new life when an empty cell has exactly three neighbours" in {
-    GameOfLife.evolve(Set((0,2), (1,2), (2,2))) should contain (1, 1)
-    GameOfLife.evolve(Set((0,0), (1,0), (2,0))) should contain (1, 1)
-    GameOfLife.evolve(Set((0,0), (0,1), (0,2))) should contain (1, 1)
-  }
-
-  "neighbours" should "calculate neighbours for cell" in {
-    neighbours (1, 1).toSet should === (Set((0,0), (1,0), (2,0), 
-                                            (0,1),        (2,1), 
-                                            (0,2), (1,2), (2,2))) 
-  }
-  
-  "occurences" should "count single occurence" in {
-    occurences(List((1,1))) should === (Map((1,1) -> 1))
-  }
-  
-  it should "count multiple occurences" in {
-    occurences(List((1,1), (2,1), (1,1))) should === (Map((1,1) -> 2, (2,1) -> 1))
-  }
 }
